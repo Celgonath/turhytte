b01e07883fcb07646e3a995e8fc3bb2eb11304c9
Stian Hatling <xorben@hotmail.com>
2014-03-07 09:39:59 +0100

    laget TouristParser-klasse

---
+++ turhytte/src/no/uib/info216/parser/TouristParser.java
@@ -0,0 +1,22 @@
+package no.uib.info216.parser;
+
+import java.io.File;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+
+public class TouristParser {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		jsonToken
+
+	}
+
+	public static void parseCabins(File f){
+
+	}
+
+}
